d1419fb720784a2c164b93d69889033bab8470be
CSpathas <spathasx@gmail.com>
2020-11-16 00:10:57 +0200

    Starting now!

--- src/main/java/CSVReaderExample.java
+++ src/main/java/CSVReaderExample.java
@@ -1,18 +1,30 @@
 import com.opencsv.CSVReader;
+//import guru.nidi.graphviz.attribute.Color;
+import guru.nidi.graphviz.attribute.Font;
+import guru.nidi.graphviz.attribute.Rank;
+//import guru.nidi.graphviz.attribute.Style;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Graph;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+//import static guru.nidi.graphviz.attribute.Attributes.attr;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.TOP_TO_BOTTOM;
+import static guru.nidi.graphviz.model.Factory.*;
+//import static guru.nidi.graphviz.model.Factory.node;
+
 public class CSVReaderExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String csvFile = "C:\\Users\\spath\\Desktop\\BaratheonTreeWithRels.csv";
-        CSVReader reader = null;
-        Set<Human> man = new HashSet<Human>();
+        CSVReader reader;
+        Set<Human> man = new HashSet<>();
         Set<Relation> rel = new HashSet<>();
 
         //CSV to Objects
@@ -20,7 +32,7 @@ public class CSVReaderExample {
             reader = new CSVReader(new FileReader(csvFile));
             String[] line;
             while ((line = reader.readNext()) != null) {
-                if(line[line.length -1] == "" || line[line.length -1] == null) {
+                if(line[line.length -1].equals("") || line[line.length -1] == null) {
                     man.add(new Human(line[0], line[1]));
                 }
                 else {
@@ -36,6 +48,24 @@ public class CSVReaderExample {
         System.out.println("________________________________________________________________\n");
         System.out.println("Relationships");
         System.out.println(rel.toString());
+
+        Graph g = graph("Baratheon").directed()
+                .graphAttr().with(Rank.dir(TOP_TO_BOTTOM))
+                .nodeAttr().with(Font.name("Arial"))
+                .linkAttr().with("class", "link-class")
+                .with(
+                        //
+                );
+
+        Graphviz
+                .fromGraph(g)
+                .height(500)
+                .render(Format.DOT).toFile(new File("./ex1.dot"));
+
+        Graphviz
+                .fromGraph(g)
+                .height(500)
+                .render(Format.PNG).toFile(new File("./ex1.png"));
 
 
     }
